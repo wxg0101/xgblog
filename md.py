620de83d28f460d42d41d71c978adb00a0fa7a8b
wxg0101 <1393691752@qq.com>
2022-01-07 18:32:32 +0800

    init

--- md.py
+++ md.py
@@ -38,5 +38,5 @@ tags:                               #标签
         )
     f.close()
     time.sleep(3)
-    os.system('D:\\"Program Files (x86)"\\Typora\\typora.exe {}'.format(
+    os.system('D:\\Typora\\typora.exe {}'.format(
         filename))
